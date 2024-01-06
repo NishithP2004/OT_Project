cdaf5ef17a40ae6ae334090339e985578b5496c3
Nishith P <nishithp11@gmail.com>
2024-01-06 19:19:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+require("dotenv").config();
+
+const multer = require("multer");
+const upload = multer({
+    limits: 50, // 50mb
+});
+
+const {
+    ChatGoogleGenerativeAI
+} = require("@langchain/google-genai");
+const { HumanMessage } = require("@langchain/core/messages")
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.static("public"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
+
+const model = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GOOGLE_GEMINI_PRO_KEY,
+    temperature: 0.4,
+    modelName: "gemini-pro-vision"
+})
+
+app.post("/solve", upload.single("image"), async (req, res) => {
+    try {
+        let imageBuffer = req.file.buffer;
+
+        const result = await solveQuestion(imageBuffer)
+        res.status(200).send({
+            success: true,
+            result: JSON.parse(result)
+        })
+    } catch(err) {
+        if(err) {
+            console.error(err)
+            res.status(500).send({
+                error: err.message,
+                success: false
+            })
+        }
+        
+    }
+
+})
+
+async function solveQuestion(image) {
+    const input = [
+        new HumanMessage({
+            content: [{
+                    type: "text",
+                    text: `SYSTEM:  You are an intelligent Math question parser.
+                                    Given the image of a question, you can intelligently identify the various constraints and the objective function and return the same in JSON format.
+                                    All the mathematical functions must be in Latex format.
+                                    Output Format: 
+                                    {
+                                        "type": "MAXIMA | MINIMA",
+                                        "objective": "" // return the objective function in normal javascript syntax
+                                        "constraints": [
+                                           "x > 0"
+                                            ...
+                                        ],
+                                        "description": "A brief description about the variables used"
+                                    }
+                                    `,
+                },
+                {
+                    type: "image_url",
+                    image_url: `data:image/png;base64,${image.toString("base64")}`,
+                },
+            ],
+        }),
+    ];
+
+    const res = await model.invoke(input);
+
+    console.log(res.content);
+
+    return (res.content.startsWith("`") == true? JSON.parse(res.content.slice("```JSON".length, -"```".length)): res.content)
+}
---
+++ public/script.js
@@ -0,0 +1,108 @@
+var elt = document.getElementById('calculator');
+var calculator = Desmos.GraphingCalculator(elt, {
+    expressions: false
+});
+var objectiveFn;
+
+let form = document.forms[0];
+
+form.addEventListener('submit', async (ev) => {
+    try {
+        ev.preventDefault();
+        document.getElementById("submit").setAttribute("disabled", true)
+        const formData = new FormData(form);
+        const imageBuffer = await formData.get("image").arrayBuffer()
+        const imageBlob = new Blob([imageBuffer], {
+            type: "image/png"
+        })
+        const objectURL = URL.createObjectURL(imageBlob);
+        const imgElement = document.getElementById("question");
+        imgElement.style.display = "block";
+        imgElement.src = objectURL;
+
+        const response = await fetch(form.action, {
+                method: form.method,
+                body: formData
+            })
+            .then((res) => {
+                return res.json();
+            })
+            .catch(err => console.error)
+
+        if (!response.success) {
+            alert("An error occurred.")
+        } else {
+            document.getElementById("reset").click();
+            solve(response.result)
+            console.log(response.result)
+        }
+    } catch (err) {
+        if (err)
+            console.error(err)
+    }
+
+    document.getElementById("submit").removeAttribute("disabled")
+    return true;
+})
+
+const solve = (data) => {
+    let steps = document.getElementById("steps");
+    steps.innerHTML =
+        `
+    <table border="1" cellspacing="10px" cellpadding="10px" class="stepsTable">
+        <thead>
+            <tr>
+                <th>Objective Function</th>
+                <th>Constraints</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr rowspan="${data["constraints"].length}">
+                <td>${data.objective}</td>
+                ${data["constraints"]?.map(c => {
+                    return `<td>${c}</td>`
+                }).join("")}
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr colspan="5">
+                <td>Goal: Find <b>${data.type}</b></td>
+            </tr>
+        </tfoot>
+    </table>
+    <p>${data.description}</p>
+    `
+
+    data.constraints.forEach((c, i) => {
+        calculator.setExpression({
+            id: 'constraint' + i,
+            latex: c
+        });
+    })
+
+    objectiveFn = data.objective;
+}
+
+calculator.observe('expressionAnalysis', function () {
+    for (var id in calculator.expressionAnalysis) {
+        var analysis = calculator.expressionAnalysis[id];
+        if (analysis.isGraphable) console.log('This expression can be plotted.');
+        if (analysis.isError)
+            console.log(`Expression '${id}': ${analysis.errorMessage}`);
+        if (analysis.evaluation) console.log(`value: ${analysis.evaluation.value}`);
+    }
+});
+
+function calc(input) {
+    console.log(input)
+    let [x, y] = input.split(", ");
+    x = parseFloat(x);
+    y = parseFloat(y);
+
+    let temp = objectiveFn.replaceAll("x", `*${x}`);
+    temp = temp.replaceAll("y", `*${y}`);
+
+    let ans = parseFloat(eval(temp));
+
+    document.getElementById("evaluationResult").innerText = ans;
+}
